dd4cf917d1d4562601df2290c0e2653bbf0186e7
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-15 19:05:23 +0100

    pari e dispari step logici

--- 1-pari e dispari/main.js
+++ 1-pari e dispari/main.js
@@ -1,1 +1,10 @@
 // Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+// step
+// funzione per generare un numero casuale tra 1 e 5
+// funzione per determinare se sia pari o dispari
+// funzione con prompt per far scegliere pari o dispari e il numero
+// if di verifica del numero per vedere se ha non ha scritto bagianate
+// generare il numero casuale per il pc
+// somma dei 2 numeri
+// e alla somma vediamo se sia pari o dispari, decretiamo il vincitore
