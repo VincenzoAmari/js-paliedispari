3e8fdef67619e36e7b346020077e16d8b338d79a
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-15 19:56:53 +0100

    step logici

--- 2-palindromo/main.js
+++ 2-palindromo/main.js
@@ -1,1 +1,8 @@
 // Palidroma Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+// step
+// funzione per verificare se è palindroma con dentro un metodo che la renda tutta minuscola e poi
+// lo split reverse e join per invertirla
+// poi un altra funzione dove chiediamo di inserire una parola e verifichiamo se sia palindroma
+
+// preghiamo...
